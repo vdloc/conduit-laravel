e68aea3e906b2b889c7a7131c527562f11ac5b79
vdloc <uvcudlco@gmail.com>
2024-09-07 13:44:03 +0700

    feat(styles): integrate Tailwind CSS with PostCSS setup
    Added Tailwind CSS and PostCSS dependencies to package.json. Created postcss.config.js and tailwind.config.js for configuration. Updated Vite config with consistent formatting. This integration enables the use of utility-first CSS framework and automatic vendor prefixing.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["Inter", "sans-serif"],
+                sen: ["Sen", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
